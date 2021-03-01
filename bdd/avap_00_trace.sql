6aa8cbd050651d56415cdb00ea672a7c6cbb72fd
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-03-01 10:34:29 +0100

    Update avap_00_trace.sql

--- bdd/avap_00_trace.sql
+++ bdd/avap_00_trace.sql
@@ -1,1 +1,19 @@
-à rédiger
+/*AVAP V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* avap_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Bodet Grégory */
+
+/*  
+ 
+  Liste des dépendances :
+  schéma          | table                 | description                                                   | usage
+*/
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2021-03-01 : GB / initialisation du code (dernière version onctionnel)
+
+
