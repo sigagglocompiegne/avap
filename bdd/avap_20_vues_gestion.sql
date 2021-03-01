b691deb2ad2b7a9272248709688e0b80ac89fdee
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-03-01 11:27:50 +0100

    Update avap_20_vues_gestion.sql

--- bdd/avap_20_vues_gestion.sql
+++ bdd/avap_20_vues_gestion.sql
@@ -1,1 +1,10 @@
-à rédiger
+/*AVAP V1.0*/
+/*Creation des vues et triggers nécessaires à la gestion via l'application web-métier */
+/*avap_20_VUES_GESTION.sql */
+/*PostGIS*/
+/* GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet*/
+
+
+-- Sans objet
+
